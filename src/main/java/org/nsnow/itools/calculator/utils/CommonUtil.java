d102ce19b768be4520023fff75e89f12576681f1
15558074069 <784069440@qq.com>
2023-04-22 08:03:23 +0800

    计算器Demo 实现了带括号的加减乘除

--- src/main/java/org/nsnow/itools/calculator/utils/CommonUtil.java
+++ src/main/java/org/nsnow/itools/calculator/utils/CommonUtil.java
@@ -3,6 +3,8 @@ package org.nsnow.itools.calculator.utils;
 import org.nsnow.itools.calculator.models.Op;
 import org.nsnow.itools.calculator.models.Pair;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -12,15 +14,17 @@ public class CommonUtil {
         return input.replaceAll("\\s", "");
     }
 
-    public static double calculate(String op,double num1,double num2){
-        if(Op.PLUS.getOp().equals(op)){
-            return num1 + num2;
-        }else if(Op.MINUS.getOp().equals(op)){
-            return num1 - num2;
-        }else if(Op.MULTIPLY.getOp().equals(op)){
-            return num1 * num2;
-        }else if(Op.DIVIDE.getOp().equals(op)){
-            return num1 / num2;
+    public static double calculate(String op, double num1, double num2) {
+        BigDecimal n1 = new BigDecimal(num1);
+        BigDecimal n2 = new BigDecimal(num2);
+        if (Op.PLUS.getOp().equals(op)) {
+            return n1.add(n2).doubleValue();
+        } else if (Op.MINUS.getOp().equals(op)) {
+            return n1.subtract(n2).doubleValue();
+        } else if (Op.MULTIPLY.getOp().equals(op)) {
+            return n1.multiply(n2).doubleValue();
+        } else if (Op.DIVIDE.getOp().equals(op)) {
+            return n1.divide(n2, 10, RoundingMode.HALF_UP).doubleValue();
         }
         throw new IllegalArgumentException("invalid operation");
     }
@@ -36,15 +40,15 @@ public class CommonUtil {
         String numberPattern = "^[0-9]*\\.?[0-9]+";
         Pattern pattern = Pattern.compile(numberPattern);
         Matcher matcher = pattern.matcher(input);
-        if(!matcher.find()){
+        if (!matcher.find()) {
             return null;
         }
         int position = matcher.end();
         String value = matcher.group(0);
-        return new Pair(value,position);
+        return new Pair(value, position);
     }
 
-    public static String cut(String input,int lastIndex){
+    public static String cut(String input, int lastIndex) {
         return input.substring(lastIndex);
     }
 
@@ -52,12 +56,12 @@ public class CommonUtil {
         String numberPattern = "[\\+\\-\\*/()]{1}";
         Pattern pattern = Pattern.compile(numberPattern);
         Matcher matcher = pattern.matcher(input);
-        if(!matcher.find()){
+        if (!matcher.find()) {
             return null;
         }
         int position = matcher.end();
         String value = matcher.group(0);
-        return new Pair(value,position);
+        return new Pair(value, position);
     }
 
 
